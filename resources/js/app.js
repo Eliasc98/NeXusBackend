d1aa92a2a07b6750dbed0f25a037cd1bd2d9ccac
Godwin <oluwadamilare.c99@gmail.com>
2025-06-08 02:23:05 +0100

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+import VueI18n from 'vue-i18n';
+import App from './App.vue';
+import routes from './routes';
+import store from './store';
+import i18n from './i18n';
+import VueMeta from 'vue-meta';
+import VueClipboard from 'vue-clipboard2';
+
+window.Vue = require('vue').default;
+const app = new Vue({
+    el: '#app',
+});
